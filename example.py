1939dc0b8f00d96427c51ab1bddae9479458edc7
Mit Bailey <mitbailey@outlook.com>
2023-10-13 16:17:52 -0400

    Initial commit.

---
+++ example.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from unpack4d import unpacker as u4d
+# import unpack4d as u4d
+
+if __name__ == '__main__':
+    surfaces, metadata = u4d.unpack('example.4d')
+
+    # Convert to nanometers
+    data = np.multiply(surfaces['SurfaceInWaves'], metadata['WavelengthInNanometers'])
+
+    FIG_W = 9
+    FIG_H = 9
+    figscale = 0.75
+
+    fig, ax = plt.subplots(1, 1, figsize=(FIG_W*figscale, FIG_H*figscale))
+    im = ax.imshow(data, cmap=plt.get_cmap('turbo'), interpolation='none')
+    plt.colorbar(im,ax=ax,fraction=0.046, pad=0.04, orientation='vertical')
+
+    fig.suptitle('Surface')
+
+    plt.show()
---
+++ unpack4d/unpacker.py
@@ -0,0 +1,46 @@
+import h5py as h5
+
+def h5dump(path, group='/'):
+    """
+    Print an HDF5 format file's internal structure including groups and object attributes.
+    
+    Group: you can give a specific group, defaults to the root group.
+    """
+    with h5.File(path,'r') as f:
+         _descend_obj(f[group])
+
+def _descend_obj(obj, sep='\t'):
+    """
+    Iterate through groups in a HDF5 file and prints the groups and datasets names and datasets attributes
+    """
+    if type(obj) in [h5._hl.group.Group,h5._hl.files.File]:
+        for key in obj.keys():
+            print (sep,'-',key,':',obj[key],obj[key].attrs.keys())
+            _descend_obj(obj[key],sep=sep+'\t')
+    elif type(obj)==h5._hl.dataset.Dataset:
+        for key in obj.attrs.keys():
+            print (sep+'\t','-',key,':',obj.attrs[key])
+
+def unpack(path):
+    if not path.endswith('.4d'):
+        print('Error: file must be a .4d file')
+        exit(1)
+
+    surfaces = {}
+    metadata = {}
+
+    hfdata = h5.File(path, 'r')
+
+    surfaces['FringeAmplitude'] = data = hfdata['Measurement']['FringeAmplitude']['Data']
+    surfaces['Intensity'] = data = hfdata['Measurement']['Intensity']['Data']
+    surfaces['Modulation'] = data = hfdata['Measurement']['Modulation']['Data']
+    surfaces['SurfaceInWaves'] = data = hfdata['Measurement']['SurfaceInWaves']['Data']
+    surfaces['UnprocessedUnwrappedPhase'] = data = hfdata['Measurement']['UnprocessedUnwrappedPhase']['Data']
+
+    metadata['WavelengthInNanometers'] = hfdata['Measurement'].attrs['WavelengthInNanometers']
+    metadata['ExposureTimeInMilliseconds'] = hfdata['Measurement']['GlobalSettings']['ACA2440'].attrs['ExposureTimeInMilliseconds']
+    metadata['Timestamp'] = hfdata['Measurement']['Metadata'].attrs['Timestamp']
+    metadata['MeasurementNumber'] = hfdata['Measurement']['Metadata'].attrs['MeasurementNumber']
+    metadata['NumberOfAveragedMeasurements'] = hfdata['Measurement']['GlobalSettings']['BurstSettings'].attrs['NumberOfAveragedMeasurements']
+
+    return surfaces, metadata
